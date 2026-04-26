555431fd22d60f441ac11fc48a463255b650d60b
SpatLyu <lyu.geosocial@gmail.com>
2026-04-27 00:00:01 +0800

    implement optimal parameters search for pc r interface

--- R/ops.R
+++ R/ops.R
@@ -1,6 +1,6 @@
 .ops_ts = \(data, target, source, E = 3, k = E+2, tau = 1, style = 1, lib = NULL, pred = NULL,
-            maximize = "positive", dist.metric = c("euclidean", "manhattan", "maximum"), zero.tolerance = max(k),
-            relative = TRUE, weighted = TRUE, threads = length(libsizes), lower.parallel = TRUE, h = 0) {
+            maximize = c("positive", "negative", "dark"), dist.metric = c("euclidean", "manhattan", "maximum"), 
+            zero.tolerance = max(k), relative = TRUE, weighted = TRUE, threads = length(libsizes), lower.parallel = TRUE, h = 0) {
   dist.metric = match.arg(dist.metric)
   dlist = .validate_var(data, target, source)
   tv = dlist[[1]]; sv = dlist[[2]]
@@ -17,8 +17,8 @@
 }
 
 .ops_lattice = \(data, target, source, E = 3, k = E+2, tau = 1, style = 1, lib = NULL, pred = NULL, 
-                 maximize = "positive", dist.metric = c("euclidean", "manhattan", "maximum"), zero.tolerance = max(k),
-                 relative = TRUE, weighted = TRUE, threads = length(libsizes), lower.parallel = TRUE, nb = NULL) {
+                 maximize = c("positive", "negative", "dark"), dist.metric = c("euclidean", "manhattan", "maximum"), 
+                 zero.tolerance = max(k), relative = TRUE, weighted = TRUE, threads = length(libsizes), lower.parallel = TRUE, nb = NULL) {
   if (is.null(nb)) nb = sdsfun::spdep_nb(data)
   dist.metric = match.arg(dist.metric)
   dlist = .validate_var(data, target, source)
@@ -36,8 +36,8 @@
 }
 
 .ops_grid = \(data, target, source, E = 3, k = E+2, tau = 1, style = 1, lib = NULL, pred = NULL,
-              maximize = "positive", dist.metric = c("euclidean", "manhattan", "maximum"), zero.tolerance = max(k),
-              relative = TRUE, weighted = TRUE, threads = length(libsizes), lower.parallel = TRUE) {
+              maximize = c("positive", "negative", "dark"), dist.metric = c("euclidean", "manhattan", "maximum"), 
+              zero.tolerance = max(k), relative = TRUE, weighted = TRUE, threads = length(libsizes), lower.parallel = TRUE) {
   dist.metric = match.arg(dist.metric)
   dlist = .validate_var(data, target, source)
   tv = dlist[[1]]; sv = dlist[[2]]
