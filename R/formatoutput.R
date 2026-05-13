39484a568ce637635ab838f3a071c0d676952e51
SpatLyu <lyu.geosocial@gmail.com>
2026-05-14 00:00:01 +0800

    extend `pc` generic with visualization capabilities

--- R/formatoutput.R
+++ R/formatoutput.R
@@ -19,6 +19,29 @@ print.pc_ops = \(x,...) {
 
 #' @export
 #' @noRd
+plot.pc_single = \(x, family = "serif", ...){
+  causdf = x$summary
+  
+  ggplot2::ggplot(causdf, ggplot2::aes(x = type, y = strength, fill = type)) +
+    ggplot2::geom_col() +
+    ggplot2::geom_text(ggplot2::aes(label = round(strength, 3)), vjust = -0.5) +
+    ggplot2::scale_y_continuous(
+      limits = c(0, max(causdf$strength) * 1.1),
+      expand = ggplot2::expansion(mult = c(0, 0.1),
+      name = "Causal Strength")) +
+    ggplot2::scale_fill_manual(values = c("dark" = "#6A0DAD", 
+                                          "negative" = "#FF4136", 
+                                          "positive" = "#0074D9")) +
+    ggplot2::theme_bw(base_family = family) +
+    ggplot2::theme(
+      panel.grid = ggplot2::element_blank(),
+      legend.position = "none",
+      axis.title.x = ggplot2::element_blank()
+    )
+}
+
+#' @export
+#' @noRd
 plot.pc_boot = \(x, family = "serif",
                  xbreaks = NULL, xlimits = NULL,
                  ybreaks = seq(0, 1, by = 0.1), 
